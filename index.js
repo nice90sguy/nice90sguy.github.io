8b6cf707192a5241d01be8da7a9f6134f66feb76
Josh Greifer <joshgreifer@gmail.com>
2024-06-21 15:25:01 +0100

    Reaction time

--- index.js
+++ index.js
@@ -27,10 +27,68 @@ const games = [
 let timerHandle = undefined;
 // Hack
 let blockClick = false;
-function makeGame(rows, cols, numPairs, pairsPerTimeout = 2, timeoutMs = 3000) {
+const reactionTimes = [];
+function histogram(data, nBins, binWidth) {
+    const hist = new Array(nBins).fill(0); // @ts-ignore
+    for (const v of data) {
+        const binNum = Math.floor(v / binWidth);
+        if (binNum >= 0 && binNum < nBins)
+            ++hist[binNum];
+    }
+    return hist;
+}
+function plotHistogram(containerEl, data) {
+    const dataMax = Math.max(...data);
+    const nBins = 60;
+    // make chart a whole multiple of 10 seconds.
+    // binWidth is a whole number of seconds in milliseconds
+    const binWidth = 1_000 * (Math.floor(dataMax / 60_000) + 1);
+    const hist = histogram(data, nBins, binWidth);
+    const maxHistValue = Math.max(...hist);
+    // Clear the container
+    containerEl.innerHTML = "";
+    // Create a reaction time bar chart, with .5s per column from 0s to max(30, histInfo.max)
+    const chart = document.createElement('div');
+    chart.className = 'chart'; // CSS class
+    chart.style.gridTemplateColumns = `repeat(${nBins}, 5px)`;
+    containerEl.appendChild(chart);
+    for (let i = 0; i < nBins; ++i) {
+        const bar = document.createElement('div');
+        bar.className = 'bar';
+        bar.style.height = `${hist[i] / maxHistValue * 100}px`;
+        bar.style.gridColumn = `${i + 1}`;
+        bar.style.gridRow = '1';
+        bar.style.transform = `translateY(${100 - hist[i] / maxHistValue * 100}px)`;
+        chart.appendChild(bar);
+        // if (i % 10 == 0) {
+        //     const xLabel = document.createElement('div');
+        //     xLabel.className = 'x-label';
+        //     xLabel.style.gridRow = '2';
+        //     xLabel.style.gridColumn = `${i + 1} / span 10`
+        //     xLabel.innerText = `${i}`;
+        //     chart.appendChild(xLabel);
+        // }
+    }
+    // Add x-axis labels
+    // const xLabels = document.createElement('div');
+    // xLabels.className = 'x-labels';
+    // containerEl.appendChild(xLabels);
+    // for (let i = 0; i < histInfo.hist.length; ++i) {
+    //     const label = document.createElement('div');
+    //     label.className = 'x-label';
+    //     label.style.width = `${barWidth}%`;
+    //     label.style.left = `${i * barWidth}%`;
+    //     label.innerText = `${(i * histInfo.max / histInfo.hist.length / 1000).toFixed(0)}`
+    //     xLabels.appendChild(label);
+    // }
+}
+function makeGame(rows, cols, numPairs, pairSize = 2, pairsPerTimeout = 2, timeoutMs = 3000) {
     const grid_len = rows * cols;
     // create a grid
     const grid = new Array(grid_len).fill(0);
+    let lastMatchTime = 0;
+    let numMatches = 0;
+    let matchingCells = [];
     const i2rc = (i) => [Math.floor(i / rows), i % rows];
     const getGridIndexWithFewestTiles = () => {
         const p = [];
@@ -53,10 +111,13 @@ function makeGame(rows, cols, numPairs, pairsPerTimeout = 2, timeoutMs = 3000) {
         // Images must always be unique
         if (emoji_idx > emojis.length - 1)
             return;
-        const sz = 120;
-        const fsz = 64;
+        const sz = Math.floor(600 / cols);
+        const fsz = Math.floor(sz / 2);
         const h = grid[i];
         const v = emojis[emoji_idx];
+        const comparator = set === emojiImgs ?
+            (a, b) => (a.firstElementChild).src === (b.firstElementChild).src
+            : (a, b) => a.innerText === b.innerText;
         // https://stackoverflow.com/questions/48419167/how-to-convert-one-emoji-character-to-unicode-codepoint-number-in-javascript
         // @ts-ignore
         console.log([...v].map(e => e.codePointAt(0).toString(16)).join(`-`)); // gives correctly 1f469-200d-2695-fe0
@@ -66,7 +127,10 @@ function makeGame(rows, cols, numPairs, pairsPerTimeout = 2, timeoutMs = 3000) {
         cellElement.classList.add('tile');
         cellElement.style.zIndex = `${1000 + h}`;
         cellElement.id = `${i2rc(i)}-${h}`;
-        cellElement.innerHTML = `<img src="imgs/${v}.png" width=${sz} height=${sz}>`;
+        if (set === emojiImgs)
+            cellElement.innerHTML = `<img src="imgs/${v}.png" width=${sz} height=${sz}>`;
+        else
+            cellElement.innerText = v;
         cellElement.style.left = c * sz + "px";
         cellElement.style.top = r * sz + "px";
         cellElement.style.width = sz + "px";
@@ -85,47 +149,68 @@ function makeGame(rows, cols, numPairs, pairsPerTimeout = 2, timeoutMs = 3000) {
         cellElement.addEventListener('click', () => {
             if (blockClick)
                 return;
-            if (elToMatch === undefined) {
+            if (matchingCells.length == 0) {
                 cellElement.classList.add("selected");
-                elToMatch = cellElement;
+                matchingCells.push(cellElement);
             }
-            else if ((cellElement.firstElementChild).src === (elToMatch.firstElementChild).src && cellElement !== elToMatch) {
-                score -= 1;
-                audioEl.pause();
-                audioEl.currentTime = 0;
-                audioEl.play();
-                clearInterval(timerHandle);
-                if (addMorePairs)
-                    timerHandle = setInterval(() => {
-                        blockClick = true;
-                        for (let np = 0; np < pairsPerTimeout; ++np)
-                            dealPair();
-                        blockClick = false;
-                    }, timeoutMs);
-                scoreEl.innerHTML = score.toFixed(0);
-                elToMatch.classList.add('rotateOut');
-                cellElement.classList.add('rotateOut');
-                elToMatch = undefined;
+            else if (!matchingCells.includes(cellElement) && comparator(cellElement, matchingCells[0])) {
+                matchingCells.push(cellElement);
+                cellElement.classList.add("selected");
+                if (++numMatches == pairSize - 1) {
+                    if (lastMatchTime != 0) {
+                        const elapsed = Date.now() - lastMatchTime;
+                        reactionTimes.push(elapsed);
+                        if (reactionTimes.length > 0) {
+                            document.querySelector('.mean-reaction-time').innerText = `Mean reaction time: ${(reactionTimes.reduce((a, b) => a + b) / reactionTimes.length / 1000).toFixed(1)}`;
+                            plotHistogram(document.querySelector('.histogram'), reactionTimes);
+                        }
+                    }
+                    lastMatchTime = Date.now();
+                    numMatches = 0;
+                    score -= 1;
+                    audioEl.pause();
+                    audioEl.currentTime = 0;
+                    audioEl.play();
+                    clearInterval(timerHandle);
+                    if (addMorePairs)
+                        timerHandle = setInterval(() => {
+                            blockClick = true;
+                            for (let np = 0; np < pairsPerTimeout; ++np)
+                                dealPair(pairSize);
+                            blockClick = false;
+                        }, timeoutMs);
+                    scoreEl.innerHTML = score.toFixed(0);
+                    for (const cell of matchingCells)
+                        cell.classList.add('rotateOut');
+                    matchingCells = [];
+                }
             }
             else {
+                for (const cell of matchingCells) {
+                    cell.classList.remove("selected");
+                    cell.classList.add('rotateBack');
+                }
+                matchingCells = [];
+                numMatches = 0;
                 cellElement.classList.add("rotateBack");
-                elToMatch.classList.add("rotateBack");
-                elToMatch.classList.remove("selected");
-                elToMatch = undefined;
             }
         });
         return;
     };
-    const dealPair = () => {
-        const firstTileIdx = getGridIndexWithFewestTiles();
-        let secondTileIndex = 0; // Value is irrelevant, gets set in do loop
+    const dealPair = (n) => {
+        const tileIndices = []; // Value is irrelevant, gets set in do loop
         // Add a tile at lowest pile
-        makeTileAtIndex(firstTileIdx);
-        // Add a matching tile anywhere except on the same grid location as the first tile
-        do {
-            secondTileIndex = Math.floor(Math.random() * grid_len);
-        } while (secondTileIndex === firstTileIdx);
-        makeTileAtIndex(secondTileIndex);
+        tileIndices.push(getGridIndexWithFewestTiles());
+        let tempIndex = 0; // Value is irrelevant, gets set in do loop
+        for (let i = 0; i < n - 1; ++i) {
+            // Add a matching tile anywhere except on the same grid location as the first tile
+            do {
+                tempIndex = Math.floor(Math.random() * grid_len);
+            } while (tileIndices.includes(tempIndex));
+            tileIndices.push(tempIndex);
+        }
+        for (const gridIndex of tileIndices)
+            makeTileAtIndex(gridIndex);
         ++emoji_idx;
         ++score;
         scoreEl.innerHTML = score.toFixed(0);
@@ -137,21 +222,22 @@ function makeGame(rows, cols, numPairs, pairsPerTimeout = 2, timeoutMs = 3000) {
         let pair = 0;
         const to = setInterval(() => {
             if (pair++ < numPairs)
-                dealPair();
+                dealPair(pairSize);
             else
                 clearInterval(to);
         }, 100);
         // for (let pair = 0; pair < numPairs; ++pair)
         //     dealPair();
     };
+    // const set = allEmojis
+    const set = emojiImgs;
     // @ts-ignore
-    // const emojis = allEmojis.slice(0,100).shuffle()
-    // const emojis = allEmojis.shuffle()
-    const emojis = emojiImgs.shuffle();
+    // const emojis = set.slice(0,100).shuffle()
+    const emojis = set.shuffle();
     let emoji_idx = 0;
     let score = 0;
     const layers = [];
-    let elToMatch = undefined;
+    // let elToMatch: HTMLDivElement | undefined = undefined
     const scoreEl = document.querySelector('#score');
     const containerEl = document.querySelector('#container');
     const audioEl = document.querySelector('audio');
@@ -162,5 +248,5 @@ function makeGame(rows, cols, numPairs, pairsPerTimeout = 2, timeoutMs = 3000) {
     containerEl.appendChild(deckElement);
     deal();
 }
-makeGame(5, 5, 50, 3, 3500);
+makeGame(8, 8, 100, 3, 3, 350000);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -28,11 +28,83 @@ const games = [
 let timerHandle: number | undefined = undefined
 // Hack
 let blockClick: boolean = false;
-function makeGame(rows: number, cols: number, numPairs: number, pairsPerTimeout = 2, timeoutMs = 3000) {
+
+const reactionTimes: number[] = []
+
+function histogram(data: number[], nBins: number, binWidth : number):  number[] {
+
+
+    const hist = new Array<number>(nBins).fill(0);    // @ts-ignore
+
+    for (const v of data) {
+        const binNum = Math.floor(v / binWidth)
+        if (binNum >= 0 && binNum < nBins)
+            ++hist[binNum];
+    }
+
+    return hist
+}
+
+function plotHistogram(containerEl: HTMLDivElement, data: number[]) {
+    const dataMax = Math.max(...data)
+
+    const nBins = 60
+    // make chart a whole multiple of 10 seconds.
+    // binWidth is a whole number of seconds in milliseconds
+    const binWidth = 1_000 * (Math.floor(dataMax / 60_000) + 1)
+    const hist = histogram(data, nBins, binWidth)
+    const maxHistValue = Math.max(...hist)
+    // Clear the container
+    containerEl.innerHTML = "";
+    // Create a reaction time bar chart, with .5s per column from 0s to max(30, histInfo.max)
+    const chart = document.createElement('div');
+    chart.className = 'chart';  // CSS class
+    chart.style.gridTemplateColumns = `repeat(${nBins}, 5px)`;
+    containerEl.appendChild(chart);
+
+    for (let i = 0; i < nBins; ++i) {
+            const bar = document.createElement('div');
+            bar.className = 'bar';
+            bar.style.height = `${hist[i] / maxHistValue * 100}px`;
+            bar.style.gridColumn = `${i + 1}`;
+            bar.style.gridRow = '1';
+            bar.style.transform = `translateY(${100 - hist[i] / maxHistValue * 100}px)`;
+
+            chart.appendChild(bar);
+            // if (i % 10 == 0) {
+            //     const xLabel = document.createElement('div');
+            //     xLabel.className = 'x-label';
+            //     xLabel.style.gridRow = '2';
+            //     xLabel.style.gridColumn = `${i + 1} / span 10`
+            //     xLabel.innerText = `${i}`;
+            //     chart.appendChild(xLabel);
+            // }
+
+
+
+     }
+    // Add x-axis labels
+    // const xLabels = document.createElement('div');
+    // xLabels.className = 'x-labels';
+    // containerEl.appendChild(xLabels);
+    // for (let i = 0; i < histInfo.hist.length; ++i) {
+    //     const label = document.createElement('div');
+    //     label.className = 'x-label';
+    //     label.style.width = `${barWidth}%`;
+    //     label.style.left = `${i * barWidth}%`;
+    //     label.innerText = `${(i * histInfo.max / histInfo.hist.length / 1000).toFixed(0)}`
+    //     xLabels.appendChild(label);
+    // }
+
+}
+function makeGame(rows: number, cols: number, numPairs: number, pairSize = 2, pairsPerTimeout = 2, timeoutMs = 3000) {
     const grid_len = rows * cols
     // create a grid
     const grid: number[] = new Array<number>(grid_len).fill(0)
 
+    let lastMatchTime = 0;
+    let numMatches = 0;
+    let matchingCells: HTMLDivElement[] = [];
     const i2rc = (i: number): [number, number] => [ Math.floor(i / rows), i % rows]
 
     const getGridIndexWithFewestTiles = (): number => {
@@ -60,10 +132,14 @@ function makeGame(rows: number, cols: number, numPairs: number, pairsPerTimeout 
         if (emoji_idx > emojis.length-1)
             return;
 
-        const sz = 120;
-        const fsz = 64;
+        const sz = Math.floor(600 / cols);
+        const fsz = Math.floor(sz / 2);
         const h = grid[i]
         const v: string = emojis[emoji_idx];
+
+        const comparator = set === emojiImgs  ?
+            (a: HTMLDivElement, b: HTMLDivElement) => (<HTMLImageElement>(a.firstElementChild)).src === (<HTMLImageElement>(b.firstElementChild)).src
+            : (a: HTMLDivElement, b: HTMLDivElement) => a.innerText === b.innerText
         // https://stackoverflow.com/questions/48419167/how-to-convert-one-emoji-character-to-unicode-codepoint-number-in-javascript
         // @ts-ignore
         console.log([...v].map(e => e.codePointAt(0).toString(16)).join(`-`)) // gives correctly 1f469-200d-2695-fe0
@@ -75,7 +151,10 @@ function makeGame(rows: number, cols: number, numPairs: number, pairsPerTimeout 
         cellElement.classList.add('tile')
         cellElement.style.zIndex = `${1000 + h}`
         cellElement.id = `${i2rc(i)}-${h}`;
-        cellElement.innerHTML = `<img src="imgs/${v}.png" width=${sz} height=${sz}>`
+        if (set === emojiImgs)
+            cellElement.innerHTML = `<img src="imgs/${v}.png" width=${sz} height=${sz}>`
+        else
+            cellElement.innerText = v
         cellElement.style.left = c * sz + "px"
         cellElement.style.top = r * sz + "px"
         cellElement.style.width = sz + "px"
@@ -96,32 +175,55 @@ function makeGame(rows: number, cols: number, numPairs: number, pairsPerTimeout 
             if (blockClick)
                 return;
 
-            if (elToMatch === undefined) {
+            if (matchingCells.length == 0) {
                 cellElement.classList.add("selected");
-                elToMatch = cellElement
-            } else if ((<HTMLImageElement>(cellElement.firstElementChild)).src === (<HTMLImageElement>(elToMatch.firstElementChild)).src && cellElement !== elToMatch) {
-                score -= 1;
-                audioEl.pause();
-                audioEl.currentTime = 0
-                audioEl.play();
-                clearInterval(timerHandle);
-                if (addMorePairs)
-                    timerHandle = setInterval(() => {
-                        blockClick = true;
-                        for (let np = 0; np < pairsPerTimeout; ++np)
-                            dealPair();
-                        blockClick = false;
+                matchingCells.push(cellElement)
+            } else if (!matchingCells.includes(cellElement) && comparator(cellElement, matchingCells[0])) {
 
-                    }, timeoutMs)
-                scoreEl.innerHTML = score.toFixed(0)
-                elToMatch.classList.add('rotateOut')
-                cellElement.classList.add('rotateOut')
-                elToMatch = undefined;
+
+                matchingCells.push(cellElement)
+                cellElement.classList.add("selected");
+                if (++numMatches == pairSize-1) {
+                    if (lastMatchTime != 0) {
+                        const elapsed = Date.now() - lastMatchTime;
+
+                        reactionTimes.push(elapsed)
+                        if (reactionTimes.length > 0) {
+                            (document.querySelector('.mean-reaction-time') as HTMLDivElement).innerText = `Mean reaction time: ${(reactionTimes.reduce((a, b) => a + b) / reactionTimes.length / 1000).toFixed(1)}`
+                            plotHistogram(document.querySelector('.histogram') as HTMLDivElement, reactionTimes)
+                        }
+                    }
+                    lastMatchTime = Date.now();
+                    numMatches = 0;
+                    score -= 1;
+                    audioEl.pause();
+                    audioEl.currentTime = 0
+                    audioEl.play();
+                    clearInterval(timerHandle);
+                    if (addMorePairs)
+                        timerHandle = setInterval(() => {
+                            blockClick = true;
+                            for (let np = 0; np < pairsPerTimeout; ++np)
+                                dealPair(pairSize);
+                            blockClick = false;
+
+                        }, timeoutMs)
+                    scoreEl.innerHTML = score.toFixed(0)
+
+                    for (const cell of matchingCells)
+                        cell.classList.add('rotateOut')
+                    matchingCells = []
+
+                }
             } else {
+                for (const cell of matchingCells) {
+                    cell.classList.remove("selected");
+                    cell.classList.add('rotateBack')
+                }
+                matchingCells = []
+                numMatches = 0;
                 cellElement.classList.add("rotateBack");
-                elToMatch.classList.add("rotateBack");
-                elToMatch.classList.remove("selected");
-                elToMatch = undefined;
+
             }
 
         })
@@ -131,18 +233,22 @@ function makeGame(rows: number, cols: number, numPairs: number, pairsPerTimeout 
 
     }
 
-    const dealPair = () => {
-        const firstTileIdx = getGridIndexWithFewestTiles()
-        let secondTileIndex = 0; // Value is irrelevant, gets set in do loop
+    const dealPair = (n: number) => {
 
+        const tileIndices: number[] = [] // Value is irrelevant, gets set in do loop
         // Add a tile at lowest pile
-        makeTileAtIndex(firstTileIdx)
+        tileIndices.push(getGridIndexWithFewestTiles())
+        let tempIndex = 0; // Value is irrelevant, gets set in do loop
 
-        // Add a matching tile anywhere except on the same grid location as the first tile
-        do {
-            secondTileIndex = Math.floor(Math.random() * grid_len)
-        } while (secondTileIndex === firstTileIdx);
-        makeTileAtIndex(secondTileIndex)
+        for (let i = 0; i < n-1; ++i) {
+            // Add a matching tile anywhere except on the same grid location as the first tile
+            do {
+                tempIndex = Math.floor(Math.random() * grid_len)
+            } while (tileIndices.includes(tempIndex));
+            tileIndices.push(tempIndex)
+        }
+        for (const gridIndex of tileIndices)
+            makeTileAtIndex(gridIndex)
         ++emoji_idx;
         ++score;
         scoreEl.innerHTML = score.toFixed(0)
@@ -158,7 +264,7 @@ function makeGame(rows: number, cols: number, numPairs: number, pairsPerTimeout 
         let pair = 0;
         const to = setInterval(() => {
             if (pair++ < numPairs)
-                dealPair();
+                dealPair(pairSize);
             else
                 clearInterval(to)
         }, 100)
@@ -167,16 +273,18 @@ function makeGame(rows: number, cols: number, numPairs: number, pairsPerTimeout 
 
     }
 
+    // const set = allEmojis
+    const set = emojiImgs
+
     // @ts-ignore
-    // const emojis = allEmojis.slice(0,100).shuffle()
-    // const emojis = allEmojis.shuffle()
-    const emojis = emojiImgs.shuffle()
+    // const emojis = set.slice(0,100).shuffle()
+    const emojis = set.shuffle()
     let emoji_idx = 0;
 
     let score = 0
 
     const layers : string[][] = []
-    let elToMatch: HTMLDivElement | undefined = undefined
+    // let elToMatch: HTMLDivElement | undefined = undefined
 
     const scoreEl= document.querySelector('#score') as HTMLDivElement;
     const containerEl = document.querySelector('#container') as HTMLDivElement;
@@ -194,7 +302,7 @@ function makeGame(rows: number, cols: number, numPairs: number, pairsPerTimeout 
 
 }
 
-makeGame(5, 5, 50, 3, 3500);
+makeGame(8, 8, 100, 3, 3, 350000);
 
 
 
